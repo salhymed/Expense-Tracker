08adbe6dae6516a73df6e1d5bd67a2ee7a069c50
salhysamed <salhy.mohamed@gmail.com>
2023-12-30 16:22:55 +0100

    adding Expense Items

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+import './Card.css'
+
+function Card({children, className}) {
+    const classes = 'card '+className;
+  return (
+    <div className={classes}>
+      {children}
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/ExpenseDate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ExpenseDate.css'
+
+function ExpenseDate({ date }) {
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{date.toLocaleString('en-US', { month: 'long' })}</div>
+      <div className='expense-date__year'>{date.toLocaleString('en-US', { day: '2-digit' })}</div>
+      <div className='expense-date__day'>{date.getFullYear()}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
